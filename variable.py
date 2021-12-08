55ad1af5c41085d5b8f3db112a7c1a2d399d4996
Navik Rana <61587450+navik03@users.noreply.github.com>
2021-12-08 11:45:25 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,23 @@
+# variable is container for a value.
+
+# first_name = "Navik"
+# last_name = "Rana"
+
+# full_name = first_name+" "+last_name
+
+# print("Hello "+full_name)
+# #print(type(name))
+
+# age = 22
+# age += 1
+# print("Your current age is "+str(age))
+# # print(type(age))
+
+# height = 250.5
+# print(height)
+# print(type(height))
+
+human = False
+# print(human)
+# print(type(human))
+print("Are you an human"+str(human))
