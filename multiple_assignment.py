325607a8edc219d4e4c99f0545f2a94a816e7995
Navik Rana <61587450+navik03@users.noreply.github.com>
2021-12-08 12:10:31 +0530

    Add files via upload

---
+++ multiple_assignment.py
@@ -0,0 +1,22 @@
+#multiple assignment = allows us to assign multiple variable in a single line
+
+#single assignment = assigning different variable in different line of code
+# name = "navik"
+# age = 21
+# attractive = True
+
+#multiple assignment
+# name,age,attractive = "navik",22,True
+
+# print(name)
+# print(age)
+# print(attractive)
+
+#if the values of varibles are same 
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
