022c833cc43daec2bf8d45f1aa0bb7feab2977b9
Navik Rana <61587450+navik03@users.noreply.github.com>
2021-12-09 11:03:28 +0530

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,16 @@
+#type casting = convert the data type of a value to another data type
+
+x = 1   #int
+y = 2.0 #float
+z = "3" #str
+
+#converting it into int 
+#to change the type just data_type(varibaleName)
+
+y = int(y)
+z = float(z)
+x = str(x)
+
+print(x)
+print(y)
+print(z)
