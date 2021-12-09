c4e40ede2d06d60e5a518bbb1633baa288dd69be
Navik Rana <61587450+navik03@users.noreply.github.com>
2021-12-09 11:26:40 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,11 @@
+#to get input use "input()"
+#the input we get is in string data type
+
+name = input("What is your name: ")
+age = int(input("what is your age: "))
+height = float(input("what is your height: "))
+
+
+print("Hello "+name)
+print("You are "+str(age))
+print("You are "+str(height)+"cm tall")
